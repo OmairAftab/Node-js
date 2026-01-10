6b3ba8ab22ece9f7dc3aceca09e1204d5c885ae4
0mairAftab <omairch035@gmail.com>
2026-01-10 11:23:30 +0500

    modules in node

---
+++ hello.js
@@ -0,0 +1,7 @@
+const math=require('./math')
+// const {add ,sub}=require('./math')
+// console.log(add(4,5));
+// console.log(sub(4,4));  //destructing kr k b use kr skte
+
+console.log(math.add(4,5));
+console.log(math.sub(4,4));
